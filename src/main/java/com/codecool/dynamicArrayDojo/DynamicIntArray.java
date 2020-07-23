61b799d1abd510c37b6d75942e70498793c639d2
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-07-23 19:50:24 +0200

    Add skeleton of methods and constructors

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,6 +2,28 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
+    private int sizeOfArray;
+
+    public DynamicIntArray(){
+        this.sizeOfArray = 10;
+    }
+
+    public DynamicIntArray(int sizeOfArray){
+        this.sizeOfArray = sizeOfArray;
+    }
+
+    public void add(int number){
+
+    }
+
+    public void remove(int indexOfNumber){
+
+    }
+
+    public void insert(int elementsToReplace, int newElements){
+
+    }
+
     @Override
     public String toString() {
         return "Implement me plz.. Pretty plz..";
