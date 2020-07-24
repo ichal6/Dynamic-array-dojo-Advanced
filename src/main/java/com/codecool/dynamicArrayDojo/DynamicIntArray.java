22c95081857e815f32faa7ef2384f5781ca5ecdb
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-07-24 11:36:12 +0200

    Add body of constructors and fields

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,15 +1,23 @@
 package com.codecool.dynamicArrayDojo;
 
+import java.util.Arrays;
+
 // put your code here!
 public class DynamicIntArray {
     private int sizeOfArray;
+    private int[] array;
+    private int index;
 
     public DynamicIntArray(){
         this.sizeOfArray = 10;
+        this.array = new int[sizeOfArray];
+        this.index = 0;
     }
 
     public DynamicIntArray(int sizeOfArray){
         this.sizeOfArray = sizeOfArray;
+        this.array = new int[sizeOfArray];
+        this.index = 0;
     }
 
     public void add(int number){
